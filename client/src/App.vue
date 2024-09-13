af411eba84da745279fe8ea36030f96564476410
Tom SALON <tom.salon-CIC@ibm.com>
2024-09-13 15:53:00 +0200

    feat(vue - core) : toasts component and store

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,38 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import ToastComponent from './components/core/toasts/ToastComponent.vue';
+import { useToastsStore } from './stores/core/toastStore';
 </script>
 
 <template>
 
   {{ $t('HELLO_WORLD') }}
 
+  <!-- Toasts -->
+  <teleport to='#toasts-container'>
+    <Transition>
+      <div class="flex flex-col items-end gap-3 z-[15000]">
+        <TransitionGroup>
+          <template v-for="toast in useToastsStore().toasts">
+            <ToastComponent :toast="toast"></ToastComponent>
+          </template>
+        </TransitionGroup>
+      </div>
+    </Transition>
+  </teleport>
+
   <RouterView/>
 </template>
+
+
+<style>
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 0.2s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+</style>
