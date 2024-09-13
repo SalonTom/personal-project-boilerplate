7812453c24957a92d243d9fddf993cd9eba42899
Tom SALON <tom.salon-CIC@ibm.com>
2024-09-13 13:19:41 +0200

    feat(vue) : tailwind init

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia';
 import { createI18n } from 'vue-i18n';
 import { traductions } from './i18n/traductions';
 
+import './main.css';
+
 const pinia = createPinia();
 
 const i18n = createI18n({
